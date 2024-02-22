cc31b72312af69df9610c42b7093191ecc2ace9b
suleman-084 <sallusk084@gmail.com>
2024-02-22 12:18:17 +0530

    sorted the array in descending order

--- dsaIntermediate/sorting.jsx
+++ dsaIntermediate/sorting.jsx
@@ -1,5 +1,5 @@
 
- 
+
 
 //bubble sort
 //given an array of integers sort the array
@@ -7,25 +7,59 @@
 //bubblseSort array = [-6,-2,4,8,20]
 
 
-const bubbleSort = (arr) => {
-    let swapped
-    do {
-        swapped = false
-        for (let i = 0; i < arr.length - 1; i++) {
-            if (arr[i] > arr[i + 1]) {
-                let temp = arr[i]
-                arr[i] = arr[i+1]
-                arr[i+1] = temp
-                swapped = true
+const binaryDiffApproach = (arr) => {
+    let n = arr.length
+    for (let i = 1; i < n; i++) {
+        for (let j = 0; j < n - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp
+
             }
+
 
         }
 
-    } while (swapped);
+    }
 
 }
-const arr = [-2,-6,20,8,4]
-bubbleSort(arr)
-console.log(arr);
+const arr1 = [-2, -6, 20, 8, 4]
+binaryDiffApproach(arr1)
+console.log(arr1);
+
+
+
+const fruits = ['orange', "apple", "orange", "kiwi", "grapes", "kiwi", "orange", "grapes", "grapes", "grapes", "orange", "orange", "banana"];
+
+// Count the occurrences of each fruit
+const fruitCount = fruits.reduce((acc, fruit) => {
+    acc[fruit] = (acc[fruit] || 0) + 1;
+    return acc;
+}, {});
+
+// Convert the fruit count object into an array of arrays
+const fruitCountArray = Object.entries(fruitCount);
+
+
+const sorting = (arr) => {
+    let n = fruitCountArray.length
+
+    for (let i = 1; i < n; i++) {
+        for (let j = 0; j < n - 1; j++) {
+            if (arr[j] < arr[j + 1]) {
+                let temp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+sorting(fruitCountArray);
+console.log(fruitCountArray);
+
+
+
+
 
 
