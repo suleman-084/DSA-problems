508f47f99e4aac033b9648fe25237964fd3dc95e
suleman-084 <sallusk084@gmail.com>
2024-02-23 18:15:42 +0530

    reverse of number problem solved

--- dsaMaths/mathematicsDsa.jsx
+++ dsaMaths/mathematicsDsa.jsx
@@ -1,10 +1,23 @@
 const countDigits = (n) => {
     let count = 0
     while(n>0) {
-        const lastDigit = n % 10
+        
         count = count + 1
         n = Math.floor(n / 10)
     }
     return count
 }
 console.log(countDigits(5589));
+
+// reverse of a number 
+
+const reverseNumber = (n) => {
+    let reverseNumber1 = 0
+    while(n>0) {
+        const lastDigit = n % 10
+        reverseNumber1 = (reverseNumber1 * 10) + lastDigit
+        n = Math.floor(n / 10)
+    }
+    return reverseNumber1
+}
+console.log(reverseNumber(5589));
