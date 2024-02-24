793bc595c445254b83ca767ed5271441f7592c34
suleman-084 <sallusk084@gmail.com>
2024-02-24 15:18:35 +0530

    divisor problem solved

--- dsaMaths/mathematicsDsa.jsx
+++ dsaMaths/mathematicsDsa.jsx
@@ -1,7 +1,7 @@
 const countDigits = (n) => {
     let count = 0
-    while(n>0) {
-        
+    while (n > 0) {
+
         count = count + 1
         n = Math.floor(n / 10)
     }
@@ -13,7 +13,7 @@ console.log(countDigits(5589));
 
 const reverseNumber = (n) => {
     let reverseNumber1 = 0
-    while(n>0) {
+    while (n > 0) {
         const lastDigit = n % 10
         reverseNumber1 = (reverseNumber1 * 10) + lastDigit
         n = Math.floor(n / 10)
@@ -27,17 +27,49 @@ const palindrome = (n) => {
     let reverseNumber = 0
     let duplicate = n
 
-    while(n>0) {
+    while (n > 0) {
         const lastDigit = n % 10
         reverseNumber = (reverseNumber * 10) + lastDigit
         n = Math.floor(n / 10)
     }
     if (duplicate === reverseNumber) {
-        console.log(true);
-        
-    }else{
-        console.log(false);
+        return true
+
+    } else {
+        return false
     }
-   
+
 }
 console.log(palindrome(121));
+
+//Armstrong number 
+
+const armStrong = (n) => {
+    let sum = 0
+    let duplicate = n
+
+    while (n > 0) {
+        const lastDigit = n % 10
+        sum = sum + (lastDigit * lastDigit * lastDigit)
+        n = Math.floor(n / 10)
+    }
+    if (sum === duplicate) {
+        return true;
+    } else {
+        return false;
+    } s
+}
+console.log(armStrong(371));
+
+
+//print all divisors
+
+const divisor = (n) => {
+    for (let i = 0; i <= n; i++) {
+        if (n % i === 0) {
+            console.log(i);
+
+        }
+    }
+}
+divisor(2)
