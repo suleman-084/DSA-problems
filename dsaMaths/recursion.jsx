23f1b216da24c706e94e3001e47a6a693a0a78f9
suleman-084 <sallusk084@gmail.com>
2024-02-28 12:55:37 +0530

    solved recursion problem with back tracks

--- dsaMaths/recursion.jsx
+++ dsaMaths/recursion.jsx
@@ -1,52 +1,53 @@
-// const naming = (n) => {
-//     let result = []
-//     for (let i = 0; i < n; i++) {
-//       result[i] = "suleman"
-//     }
-//     return result
-// }
-// // console.log("names",naming(5) );
+const naming = (n) => {
+    let result = []
+    for (let i = 0; i < n; i++) {
+      result[i] = "suleman"
+    }
+    return result
+}
+// console.log("names",naming(5) );
 
-// const recursive = (i,n) => {
-//     if(i > n) {
-//         return //basecase
-//     }else {
-//         // console.log("suleman");// for printing 5 names 
-//         console.log(i); // for printing linearly from 1 to n
-//         recursive(i + 1 ,n)
-//     }
-// }
-// recursive(1,4)
+const recursive = (i,n) => {
+    if(i > n) {
+        return //basecase
+    }else {
+        // console.log("suleman");// for printing 5 names 
+        console.log(i); // for printing linearly from 1 to n
+        recursive(i + 1 ,n)
+    }
+}
+recursive(1,4)
 
 // print from n to 1 
 
-// const nonLinearly = (i, n) => {
-//     if (i < 1) {
-//         return
-//     } else {
-//         console.log(i);
-//         nonLinearly(i - 1, n)
+const nonLinearly = (i, n) => {
+    if (i < 1) {
+        return
+    } else {
+        console.log(i);
+        nonLinearly(i - 1, n)
 
 
-//     }
-// }
-// nonLinearly(1, 5)
+    }
+}
+nonLinearly(1, 5)
 
 
-//print 1 to n without using  i +1 with backtrack
+// print 1 to n without using  i +1 with backtrack
 
-// const backtrack = (i, n) => {
-//     if (i < 1) {
-//         return
-//     } else {
+const backtrack = (i, n) => {
+    if (i < 1) {
+        return
+    } else {
 
-//         backtrack(i - 1, n)
-//         console.log(i);
+        backtrack(i - 1, n)
+        console.log(i);
 
-//     }
-// }
-// backtrack(5, 5) //o/p = 1 2 3 4 5
+    }
+}
+backtrack(5, 5) //o/p = 1 2 3 4 5
 
+// print n to 1 without using  i -1 with backtrack
 const numberTrack = (i,n) => {
     if (i > n) {
         return    
