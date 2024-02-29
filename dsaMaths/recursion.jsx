fb156bd480d039efa2f8bfcf15b9cc1817e720cc
suleman-084 <sallusk084@gmail.com>
2024-02-29 11:37:37 +0530

    factorail of a number problem solved

--- dsaMaths/recursion.jsx
+++ dsaMaths/recursion.jsx
@@ -1,61 +1,95 @@
-const naming = (n) => {
-    let result = []
-    for (let i = 0; i < n; i++) {
-      result[i] = "suleman"
-    }
-    return result
-}
-// console.log("names",naming(5) );
+// const naming = (n) => {
+//     let result = []
+//     for (let i = 0; i < n; i++) {
+//       result[i] = "suleman"
+//     }
+//     return result
+// }
+// // console.log("names",naming(5) );
 
-const recursive = (i,n) => {
-    if(i > n) {
-        return //basecase
-    }else {
-        // console.log("suleman");// for printing 5 names 
-        console.log(i); // for printing linearly from 1 to n
-        recursive(i + 1 ,n)
-    }
-}
-recursive(1,4)
+// const recursive = (i,n) => {
+//     if(i > n) {
+//         return //basecase
+//     }else {
+//         // console.log("suleman");// for printing 5 names 
+//         console.log(i); // for printing linearly from 1 to n
+//         recursive(i + 1 ,n)
+//     }
+// }
+// recursive(1,4)
 
-// print from n to 1 
+// // print from n to 1 
 
-const nonLinearly = (i, n) => {
-    if (i < 1) {
-        return
-    } else {
-        console.log(i);
-        nonLinearly(i - 1, n)
-
-
-    }
-}
-nonLinearly(1, 5)
+// const nonLinearly = (i, n) => {
+//     if (i < 1) {
+//         return
+//     } else {
+//         console.log(i);
+//         nonLinearly(i - 1, n)
 
 
-// print 1 to n without using  i +1 with backtrack
+//     }
+// }
+// nonLinearly(1, 5)
 
-const backtrack = (i, n) => {
-    if (i < 1) {
-        return
-    } else {
 
-        backtrack(i - 1, n)
-        console.log(i);
+// // print 1 to n without using  i +1 with backtrack
 
-    }
-}
-backtrack(5, 5) //o/p = 1 2 3 4 5
+// const backtrack = (i, n) => {
+//     if (i < 1) {
+//         return
+//     } else {
 
-// print n to 1 without using  i -1 with backtrack
-const numberTrack = (i,n) => {
-    if (i > n) {
-        return    
-    }
-    else {
-        numberTrack(i + 1, n)
-            console.log(i);
+//         backtrack(i - 1, n)
+//         console.log(i);
+
+//     }
+// }
+// backtrack(5, 5) //o/p = 1 2 3 4 5
+
+// // print n to 1 without using  i -1 with backtrack
+// const numberTrack = (i,n) => {
+//     if (i > n) {
+//         return    
+//     }
+//     else {
+//         numberTrack(i + 1, n)
+//             console.log(i);
        
+//     }
+// }
+// numberTrack(1,4)
+
+//sum of first n number n=3 o/p = 6
+//parameterised way
+
+
+const parameterisedWay =(i,sum) => {
+    if (i < 1) {
+        console.log(sum);
+        return 
+    }else {
+        parameterisedWay(i-1, sum+i)
+    }
+
+}
+parameterisedWay(3,0)
+
+const functionalWay = (n) => {
+    if (n===0) {
+        return 0        
+    }else {
+    return n + functionalWay( n-1)
     }
 }
-numberTrack(1,4)
+console.log(functionalWay(4));
+
+
+const factorial = (n) => {
+    if (n===0) {return 1
+        
+    }else {
+        return n * factorial(n-1)
+    }
+}
+console.log(factorial(4));
